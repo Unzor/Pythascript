e9f2ac8901a65baa659c305137282ee1a3afd22a
Unzor <71789103+Unzor@users.noreply.github.com>
2021-02-03 17:29:49 -0600

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,5 @@
+print("Hello!")
+print("This message is made by Pythascript.")
+print("There are some things you cannot do with Pythascript, such as input(),")
+print("but you can still do many things with Pythascript!")
+print("Good luck on your first voyage!")
